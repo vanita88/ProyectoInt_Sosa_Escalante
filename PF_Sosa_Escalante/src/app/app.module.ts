6f6d786238ee3b773cb6d7b6a123c0c226a9bdbf
vanita88 <silvana.sescalante@gmail.com>
2022-05-12 00:47:21 -0300

    trabajando imagen bootstrap

--- PF_Sosa_Escalante/src/app/app.module.ts
+++ PF_Sosa_Escalante/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './componentes/head/head.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
+import { SocialesComponent } from './componentes/sociales/sociales.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    LogoAPComponent,
+    SocialesComponent,
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
